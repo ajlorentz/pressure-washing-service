dd7748ff48f255bd310c95616f46f5630d0a3ca6
MichaelDFaust <125297808+MichaelDFaust@users.noreply.github.com>
2023-04-30 15:15:43 -0500

    Added descriptions to elements in equipment-data list of equipment

--- src/app/equipment-data.service.ts
+++ src/app/equipment-data.service.ts
@@ -6,24 +6,28 @@ export class EquipmentDataService {
     {
       id: 1,
       name: 'Lightweight Washer',
+      description: 'Gentle washer used for light cleaning',
       cost: 30,
     },
 
     {
       id: 2,
       name: 'Medium-Duty Washer',
+      description: 'Standard washer for normal cleaning',
       cost: 50,
     },
 
     {
       id: 3,
       name: 'Heavy-Duty Washer',
+      description: 'Forceful washer for tough, stubborn cleaning',
       cost: 70,
     },
 
     {
       id: 4,
       name: 'Ultra-Strength Washer',
+      description: 'Extremely forceful washer for crusted, engrained cleaning',
       cost: 130,
     },
   ];
