4c4f5c683a1d9d6bdb320fb42368477d8ba35fad
MichaelDFaust <125297808+MichaelDFaust@users.noreply.github.com>
2023-04-30 15:00:37 -0500

    Added list of elements and getter/setter methods to equipment service

--- src/app/equipment-data.service.ts
+++ src/app/equipment-data.service.ts
@@ -2,5 +2,41 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class EquipmentDataService {
+  equipment = [
+    {
+      id: 1,
+      name: 'Lightweight Washer',
+      cost: 30,
+    },
+
+    {
+      id: 2,
+      name: 'Medium-Duty Washer',
+      cost: 50,
+    },
+
+    {
+      id: 3,
+      name: 'Heavy-Duty Washer',
+      cost: 70,
+    },
+
+    {
+      id: 4,
+      name: 'Ultra-Strength Washer',
+      cost: 130,
+    },
+  ];
+
+  costOfSelectedWasher: number = 0;
+
+  getCostOfSelectedWasher() {
+    return this.costOfSelectedWasher;
+  }
+
+  setCostOfSelectedWasher(cost: number) {
+    this.costOfSelectedWasher = cost;
+  }
+
   constructor() {}
 }
