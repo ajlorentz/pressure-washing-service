1f2fadc72cf73d51feec1d686d239b958ba830f8
ajlorentz <125420563+ajlorentz@users.noreply.github.com>
2023-05-01 04:05:25 -0500

    changed tiles in home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,6 +20,7 @@ export class HomeComponent implements OnInit {
   }
 
   tiles: Tile[] = [
+    {text: '', cols: 4, rows: 2, color: ''},
     {text: '', cols: 3, rows: 1, color: ''},
     {text: `
       <h2>Contact Us</h2>
@@ -27,9 +28,9 @@ export class HomeComponent implements OnInit {
       <p>If you have any questions or feedback, please feel free to reach out to us:</p>
   
       <ul>
-        <li><strong>Email:</strong> info@company.com</li>
-        <li><strong>Phone:</strong> (555) 555-5555</li>
-        <li><strong>Address:</strong> 123 Main St, Anytown, USA</li>
+        <li><strong>Email:</strong> pwservice@company.com</li>
+        <li><strong>Phone:</strong> (701) 777-7700</li>
+        <li><strong>Address:</strong> 123 Main St, Fargo, ND</li>
       </ul>
     `, cols: 1, rows: 2, color: 'lightgreen'},
     {text: '', cols: 1, rows: 1, color: ''},
