6b931f5b91e53b87ff5635797150dbdd63ccee5a
MichaelDFaust <125297808+MichaelDFaust@users.noreply.github.com>
2023-05-01 15:29:27 -0500

    Added html to equipment component

---
+++ src/app/equipment/equipment.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { EquipmentDataService } from '../equipment-data.service';
+
+@Component({
+  selector: 'app-equipment',
+  templateUrl: './equipment.component.html',
+  styleUrls: ['./equipment.component.css'],
+})
+export class EquipmentComponent implements OnInit {
+  constructor(private equipments: EquipmentDataService) {}
+
+  @Input() equipment: {
+    id: number;
+    name: string;
+    description: string;
+    cost: number;
+  };
+
+  ngOnInit() {}
+}
