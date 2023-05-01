8a443d55f28af2ddcb3cfc1edac47d24cfc4f30a
MichaelDFaust <125297808+MichaelDFaust@users.noreply.github.com>
2023-05-01 11:49:53 -0500

    Created scheduling-service component

---
+++ src/app/scheduling-service/scheduling-service.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scheduling-service',
+  templateUrl: './scheduling-service.component.html',
+  styleUrls: ['./scheduling-service.component.css'],
+})
+export class SchedulingServiceComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
