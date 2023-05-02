26a17fe7fde2b2b3419ecfe3a4dfa20e7442816e
ajlorentz <125420563+ajlorentz@users.noreply.github.com>
2023-05-02 15:34:43 -0500

    added about tile, added images, and changed tiles in home.component.ts;

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,18 +20,28 @@ export class HomeComponent implements OnInit {
   }
 
   tiles: Tile[] = [
-    {text: '', cols: 4, rows: 2, color: ''},
-    {text: `
-    <img src="images/home-washing1.jpg" alt="washing">
-    `, cols: 4, rows: 3, color: 'lightgreen'},
+    {text: '', cols: 4, rows: 1, color: ''},
+    {text: `<img src="https://media.istockphoto.com/id/1336188285/photo/exterior-cleaning-work-in-progress.jpg?s=612x612&w=0&k=20&c=7dQgl7S4Sz6AETT5ycw4fockeSKnjYQfmFcg0QZwYaU=" alt="washing siding">`, cols: 4, rows: 4, color: 'lightgreen'},
     {text: '', cols: 4, rows: 3, color: ''},
     {text: `
     <h2>About</h2>
 
-    <p>This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us This is where we will have all the information about us </p>
+    <p>Welcome to our pressure washing company! We are a team of dedicated professionals who are passionate about delivering top-quality pressure washing services to our clients.</p>
 
-  `, cols: 3, rows: 4, color: 'lightgreen'},
-    {text: '', cols: 1, rows: 5, color: ''},
+    <p>Our company was founded on the belief that a clean exterior is not only aesthetically pleasing, but also helps to maintain the structural integrity of your property. With years of experience in the pressure washing industry, we have honed our skills and techniques to provide the best possible results for our clients.</p>
+    
+    <p>We take pride in our attention to detail, and we always go the extra mile to ensure that every surface we clean is spotless. Whether you need your driveway, deck, or siding cleaned, we have the tools and expertise to get the job done right.</p>
+    
+    <p>At our company, we understand that every client has unique needs and expectations. That's why we take the time to listen to your concerns and tailor our services to meet your specific requirements. We also believe in transparent pricing, so you can always expect a fair and honest quote for our services.</p>
+    
+    <p>Our team consists of friendly and professional technicians who are dedicated to delivering exceptional customer service. We strive to create a positive experience for our clients from start to finish, and we're always happy to answer any questions you may have along the way.</p>
+    
+    <p>We are fully licensed and insured, and we adhere to all safety regulations to ensure a safe and efficient service. We also use eco-friendly cleaning products, so you can feel good about the impact our services have on the environment.</p>
+    
+    <p>Thank you for considering our pressure washing company for your cleaning needs. We look forward to helping you achieve a cleaner, brighter exterior for your home or business.</p>
+
+  `, cols: 3, rows: 9, color: 'lightgreen'},
+    {text: '', cols: 1, rows: 6, color: ''},
     {text: '', cols: 5, rows: 1, color: ''},
     {text: `
       <h2>Contact Us</h2>
@@ -45,7 +55,9 @@ export class HomeComponent implements OnInit {
       </ul>
     `, cols: 3, rows: 2, color: 'lightgreen'},
     {text: '', cols: 4, rows: 1, color: ''},
-    {text: '', cols: 4, rows: 3, color: 'lightgreen'},
+    {text: `<img src="https://media.istockphoto.com/id/1306260764/photo/man-cleaning-red-conrete-pavement-block-using-high-pressure-water-cleaner-paving-cleaning.jpg?s=612x612&w=0&k=20&c=Rfqp12b1yGUiOhHGF86HmqtGlfotrtIebnDgw_LuXAQ=" alt="washing brick">`, cols: 4, rows: 4, color: 'lightgreen'},
+    {text: '', cols: 12, rows: 1, color: ''},
+    {text: '', cols: 12, rows: 1, color: '#198754'},
   ];
   
 
