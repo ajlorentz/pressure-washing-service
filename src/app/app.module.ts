a56ce05fe26699101b7060faaacc3271ec9b179a
ajlorentz <125420563+ajlorentz@users.noreply.github.com>
2023-05-02 16:24:13 -0500

    added gallery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, GalleryComponent],
   imports: [BrowserModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent],
