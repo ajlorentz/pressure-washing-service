8eeba2381c4b067466a16d21885becf1e89cd176
ajlorentz <125420563+ajlorentz@users.noreply.github.com>
2023-05-02 21:43:36 -0500

    added httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,7 @@ import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, GalleryComponent],
-  imports: [BrowserModule, MatGridListModule, AppRoutingModule],
+  imports: [BrowserModule, MatGridListModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
