125a6e402f7647d50bc7d2a759a7fbe9585f5038
ajlorentz <125420563+ajlorentz@users.noreply.github.com>
2023-05-04 01:52:51 -0500

    added routing to scheduling

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,10 +4,13 @@ import { HomeComponent } from '../home/home.component';
 import { GalleryComponent } from '../gallery/gallery.component';
 import { ReviewMainComponent } from '../review-main/review-main.component';
 import { AddReviewComponent } from '../add-review/add-review.component';
+import { SchedulingComponent } from '../scheduling/scheduling.component';
+
 
 const routes: Routes = [
   { path: 'gallery', component: GalleryComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'schedule-cleaning', component: SchedulingComponent },
   { path: 'reviews', component: ReviewMainComponent },
   { path: 'reviews/add', component: AddReviewComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
