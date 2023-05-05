97fbe9aa48acb1ee7cb6b363979e8125ec46ee58
SamTheGriff <78872518+SamTheGriff@users.noreply.github.com>
2023-05-05 18:18:02 -0500

    added a confirmation alert for submitting an application

--- src/app/employment/employment.component.ts
+++ src/app/employment/employment.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employment.component.css'],
 })
 export class EmploymentComponent implements OnInit {
+  formData: any = {};
+  submitted: boolean = false;
   constructor() {}
-  onSubmit() {}
+  onSubmit() {
+    this.submitted = true;
+  }
   ngOnInit() {}
 }
